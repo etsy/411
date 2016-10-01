a9b19eaaf75e5ad30fd1073a19ef95e527b6f6a8
k_ <z@kwi.li>
2016-09-30 23:57:22 -0400

    Bugfix: Remove reference to `Push_Search`.

--- bin/generate_release.sh
+++ bin/generate_release.sh
@@ -6,6 +6,7 @@ bin/composer.phar update && \
 grunt prod && \
 tar cvzf release.tgz \
   --exclude-vcs \
+  --exclude bin/composer.phar \
   --exclude bin/generate_release.sh \
   --exclude htdocs/index-src.html \
   --exclude htdocs/assets \

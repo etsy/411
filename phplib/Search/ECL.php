e7aadc9bf81c55ec0128ebae05e10e807366bc95
k_ <z@kwi.li>
2016-10-11 13:36:06 -0400

    Extract timestamp from logs when running an ECL search.
    To do this, we log every data source that we use and pull out their
    `date_field`. If there are any matching fields in results, we use it.

--- phplib/Search/ECL.php
+++ phplib/Search/ECL.php
@@ -48,7 +48,7 @@ class ECL_Search extends Search {
         $table['to'] = $date;
         $table['from'] = $from;
 
-        $es_builder = new \ECL\Command\Elasticsearch\Builder;
+        $es_builder = new Builder;
         $es_builder->setSources(Config::get('elasticsearch'));
         $es_builder->setSettings($settings);
         $parser = new \ECL\Parser;
@@ -58,13 +58,23 @@ class ECL_Search extends Search {
         $sch = new \ECL\Scheduler;
         $results = $sch->process($statementlist);
 
+        $date_fields = $es_builder->getDateFields();
+
         $alerts = [];
         foreach($results as $result) {
             foreach($result->getAll() as $entry) {
                 $alert = new Alert;
                 $alert_date = $date;
 
-                if (array_key_exists($date_field, $entry)) {
+                $has_date_field = false;
+                foreach($date_fields as $date_field) {
+                    if(array_key_exists($date_field, $entry)) {
+                        $has_date_field = true;
+                        break;
+                    }
+                }
+
+                if ($has_date_field) {
                     // Extract the date field.
                     if(ctype_digit($entry[$date_field])) {
                         $alert_date = (int) $entry[$date_field];
@@ -80,5 +90,25 @@ class ECL_Search extends Search {
         }
 
         return $alerts;
+    }
+}
+
+class Builder extends \ECL\Command\Elasticsearch\Builder {
+    private $date_fields = [];
+
+    public function build($source, array $query=[], $agg=null, array $settings=[]) {
+        $cfg = Util::get($this->sources, $source);
+        $date_field = Util::get($cfg, 'date_field');
+
+        if(!is_null($date_field)) {
+            $this->date_fields[] = $date_field;
+            $this->date_fields = array_unique($this->date_fields);
+        }
+
+        return parent::build($source, $query, $agg, $settings);
+    }
+
+    public function getDateFields() {
+        return $this->date_fields;
     }
 }

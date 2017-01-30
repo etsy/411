c7ecd7dcce27a8fa6e595818c65c32f2734677fb
k_ <z@kwi.li>
2017-01-30 12:22:15 -0500

    Keep newlines in error messages.

--- htdocs/assets/js/app.js
+++ htdocs/assets/js/app.js
@@ -161,7 +161,7 @@ define(function(require) {
 
             var level = Util.getLevel(lvl);
             var message_elem = $(Templates['message']({
-                level: level, message: str
+                level: level, message: str.replace(/\n/g, '<br>')
             }));
             this.Elem.Message.append(message_elem);
             message_elem.delay(3000).slideUp(message_elem.remove);

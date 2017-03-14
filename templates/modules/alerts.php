29c03db4501d1520d39acb5e87458f850c435b84
k_ <z@kwi.li>
2017-03-13 23:16:56 -0400

    UI: Rearrange alerts html
    This moves the actual content of emails closer to the top, increasing
    the chance it'll show up in the email preview.

--- templates/modules/alerts.php
+++ templates/modules/alerts.php
@@ -1,7 +1,18 @@
 <?php namespace FOO; ?>
 
-<div style="<?= $panel_style ?>">
+<div style="<?= $panel_style ?>; display: table; width: 100%">
 
+  <div style="<?= $table_container_style ?>">
+
+    <?php if($vertical): ?>
+      <?php require(__DIR__ . '/alert_list.php'); ?>
+    <?php else: ?>
+      <?php require(__DIR__ . '/alert_table.php'); ?>
+    <?php endif ?>
+
+  </div>
+
+  <div style="display:table-header-group;">
   <h2 style="<?= $panel_content_style ?>">
     <a style="<?= $link_style ?>" href="<?= $base_url ?>/search/<?= $search['id'] ?>"><?= Util::escape($search['name']) ?></a>
     <small style="<?= $sub_style ?>">[<?= count($alerts) ?> Alert<?= count($alerts) != 1 ? 's':'' ?>]</small>
@@ -15,15 +26,6 @@
       <b>Time range: </b><?= $search['range'] ?> minute(s)
     <?php endif ?>
   </p>
-
-  <div style="<?= $table_container_style ?>">
-
-    <?php if($vertical): ?>
-      <?php require(__DIR__ . '/alert_list.php'); ?>
-    <?php else: ?>
-      <?php require(__DIR__ . '/alert_table.php'); ?>
-    <?php endif ?>
-
-  </div>
+</div>
 
 </div>

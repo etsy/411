ad1b890cff5d6b90685357ab9e61e448885c8f57
k_ <z@kwi.li>
2017-06-19 13:19:59 -0400

    Add autoloader for testbootstrap

--- tests/testbootstrap.php
+++ tests/testbootstrap.php
@@ -1,5 +1,17 @@
 <?php
 
-require(__DIR__ . '/TestHelper.php');
-require(__DIR__ . '/TestCase.php');
+spl_autoload_register(function($class) {
+    // Determine the file containing the class and attempt to load it.
+    $path = implode('/', array_reverse(explode('_', $class)));
+    $class_file_list = [
+        sprintf('%s/tests/%s.php', BASE_DIR, $path),
+    ];
+    foreach($class_file_list as $class_file) {
+        if(file_exists($class_file)) {
+            include_once($class_file);
+            return;
+        }
+    }
+});
+
 require(__DIR__ . '/../phplib/411bootstrap.php');

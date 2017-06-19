da681fd9ccb1f35f4e934fd8181b2f32eff04404
k_ <z@kwi.li>
2017-06-19 13:13:56 -0400

    Reorder test includes

--- tests/testbootstrap.php
+++ tests/testbootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-require(__DIR__ . '/../phplib/411bootstrap.php');
 require(__DIR__ . '/TestHelper.php');
 require(__DIR__ . '/TestCase.php');
+require(__DIR__ . '/../phplib/411bootstrap.php');

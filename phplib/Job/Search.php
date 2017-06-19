4576a1c9cb09091dd35e908b0431812ff9b4378d
k_ <z@kwi.li>
2017-06-19 11:49:39 -0400

    Fix rest of tests

--- phplib/Job/Search.php
+++ phplib/Job/Search.php
@@ -69,7 +69,9 @@ class Search_Job extends Job {
 
         $filters = $search->getFilters();
         $targets = $search->getTargets();
-        array_unshift($targets, new DB_Target);
+        if(!Util::isTesting()) {
+            array_unshift($targets, new DB_Target);
+        }
 
         // Take each Alert result and pass it through the pipeline.
         list($final_alerts, $new_errors) = $this->processFilters($alerts, $filters, $this->obj['target_date']);
